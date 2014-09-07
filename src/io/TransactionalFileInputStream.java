63d8b6a0a0720091724b3238d9dd52142dcea320
Druhin Sagar Goel <dsgoel@andrew.cmu.edu>
2014-09-07 17:57:20 -0400

    more IO

--- src/io/TransactionalFileInputStream.java
+++ src/io/TransactionalFileInputStream.java
@@ -1,5 +1,17 @@
 package io;
 
-public class TransactionalFileInputStream {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
 
+
+
+public class TransactionalFileInputStream extends InputStream implements Serializable {
+
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
--- src/io/TransactionalFileOutputStream.java
+++ src/io/TransactionalFileOutputStream.java
@@ -1,5 +1,15 @@
 package io;
 
-public class TransactionalFileOutputStream {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Serializable;
+
+public class TransactionalFileOutputStream extends OutputStream implements Serializable{
+
+	@Override
+	public void write(int b) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
